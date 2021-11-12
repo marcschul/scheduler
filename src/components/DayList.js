d32c52589064f0f2186ad9de511d1a88d1419add
Marc Schulmeister <marcschulm@gmail.com>
2021-11-12 13:48:24 -0800

    add controlled list to DayList and InterviewerList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,12 +4,12 @@ import DayListItem from 'components/DayListItem';
 export default function DayList (props) {
 
   const listOfDays = props.days.map((day) => {
-    return < DayListItem         
-    key={day.id}
-    name={day.name} 
-    spots={day.spots} 
-    selected={day.name === props.day}
-    setDay={props.setDay} 
+    return <DayListItem
+      key={props.id} 
+      name={props.name} 
+      spots={props.spots} 
+      selected={props.name === props.value}
+      setDay={() => props.onChange(props.name)}
     />
   })
 
