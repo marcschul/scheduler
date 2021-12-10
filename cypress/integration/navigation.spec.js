74c0cb86a7d41522545c666b1342f2d9bc7e7c36
Marc Schulmeister <marcschulm@gmail.com>
2021-12-09 16:42:03 -0800

    complete cypress tests

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,16 @@
+// ignore cy object for eslint
+/*global cy*/
+
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+  
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+  });
+});
